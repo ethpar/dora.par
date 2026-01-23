ccb6823e5338792fc5291fb786b649e4d846cd60
Aleksey <yudin_al_vl@mail.ru>
2026-01-23 14:51:02 +0300

    code cleanup

--- indexer/beacon/alert.go
+++ indexer/beacon/alert.go
@@ -17,44 +17,41 @@ type alertsSender struct {
 }
 
 func newAlertsSender(indexer *Indexer) *alertsSender {
-	epochAlert := dbtypes.EpochAlertState{}
-	db.GetExplorerState("alert.epoch", &epochAlert)
 	emailConfig := utils.Config.Email
-	indexer.logger.Infof("epochvotes save to emailConfig: %v", emailConfig)
-	indexer.logger.Infof("epochvotes save to db: %v", epochAlert)
+	indexer.logger.Infof("emailConfig: %v", emailConfig)
 	/*return &alertsSender{
 		indexer: indexer,
 	}*/
 	d := &alertsSender{
 		indexer: indexer,
 	}
-	d.sendEmail(uint64(1), uint64(2))
+	//d.sendEmail(uint64(1), uint64(2))
 	return d
 }
 
-func (al *alertsSender) sendAlert(tx *sqlx.Tx, epoch phase0.Epoch, blocks []*Block, epochStats *EpochStats, epochVotes *EpochVotes) {
+func (al *alertsSender) checkAndSendAlert(tx *sqlx.Tx, epoch phase0.Epoch, blocks []*Block, epochStats *EpochStats, epochVotes *EpochVotes) {
 
 	epochAlert := dbtypes.EpochAlertState{}
 	db.GetExplorerState("alert.epoch", &epochAlert)
-	al.indexer.logger.Infof("epochvotes save to db: %v", epochAlert)
-	al.indexer.logger.Infof("epochvotes save to db: %v %v", epochVotes.TargetVotePercent, epochVotes.TotalVotePercent)
+	al.indexer.logger.Infof("epochAlert: %v", epochAlert)
+	al.indexer.logger.Infof("epochAlert: epoch: %v vote: %v", epoch, epochVotes.TotalVotePercent)
 	epochAlert.Epoch = uint64(epoch)
-	var p = 100
-	if epochVotes.TargetVotePercent <= 90 {
-		p = 90
-	} else if epochVotes.TargetVotePercent <= 95 {
-		p = 95
-	} else if epochVotes.TargetVotePercent <= 98 {
-		p = 98
+	var vote = 100
+	if epochVotes.TotalVotePercent <= 90 {
+		vote = 90
+	} else if epochVotes.TotalVotePercent <= 95 {
+		vote = 95
+	} else if epochVotes.TotalVotePercent <= 98 {
+		vote = 98
 	}
-	if p != epochAlert.Percent {
-		epochAlert.Percent = p
+	if vote != epochAlert.Percent {
+		epochAlert.Percent = vote
 		db.SetExplorerState("alert.epoch", epochAlert, tx)
-		al.sendEmail(uint64(epoch), uint64(p))
+		al.sendEmail(uint64(epoch), uint64(vote))
 	}
 }
 
-func (al *alertsSender) sendEmail(epoch uint64, percent uint64) {
+func (al *alertsSender) sendEmail(epoch uint64, vote uint64) {
 	if !utils.Config.Email.Enabled {
 		return
 	}
@@ -66,12 +63,12 @@ func (al *alertsSender) sendEmail(epoch uint64, percent uint64) {
 		al.indexer.logger.Fatal(err)
 	}
 
-	var emailsOnly []string     // Для smtp.SendMail
-	var formattedNames []string // Для заголовка To:
+	var emailsOnly []string
+	var formattedNames []string
 
 	for _, addr := range addrList {
 		emailsOnly = append(emailsOnly, addr.Address)
-		formattedNames = append(formattedNames, addr.String()) // вернет "Ivan <a@ex.com>"
+		formattedNames = append(formattedNames, addr.String())
 	}
 
 	smtpHost := "smtp.gmail.com"
@@ -82,8 +79,8 @@ func (al *alertsSender) sendEmail(epoch uint64, percent uint64) {
 
 	toHeader := "To: " + strings.Join(formattedNames, ", ") + "\r\n"
 	subject := utils.Config.Email.Subject
-	subject = subject + " " + strconv.FormatUint(epoch, 10) + "percent " + strconv.FormatUint(percent, 10) + "\n"
-	body := "Epoch " + strconv.FormatUint(epoch, 10) + " voted " + strconv.FormatUint(percent, 10) + "  " +
+	subject = subject + " " + strconv.FormatUint(epoch, 10) + " voted " + strconv.FormatUint(vote, 10) + "%\n"
+	body := "Epoch " + strconv.FormatUint(epoch, 10) + " voted " + strconv.FormatUint(vote, 10) + "%  " +
 		doraUrl + "/epoch/" + strconv.FormatUint(epoch, 10) +
 		" .\n"
 	msg := []byte(toHeader + subject + body)
